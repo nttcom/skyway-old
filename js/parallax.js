fc3aa6ad5091e2f9f4b5ad8ace588c48f927d0a7
Ryosuke Otsuya <r.otsuya@gmail.com>
2013-12-03 20:14:21 +0900

    20:10のDropboxの内容に差し替えます。

--- js/parallax.js
+++ js/parallax.js
@@ -6,11 +6,11 @@ $(function(){
 	var box3Pos = $('#footer').offset();
 
 	
-	$("#slide1 > div.container > #content >h1>img").delay( 300 ).animate({ opacity: '1'},{duration:1700, easing:'easeInOutSine' });
-	$("#slide1 > div.container > #content > .catch  ").delay( 1200 ).animate({ opacity: '1'},{duration:1900, easing:'easeInOutSine' });
-	$("#slide1 > div.container > #content > .catch  > div.pointWrap > h3").delay( 2200 ).animate({ opacity: '1',marginLeft:'0'},{ duration:600,easing:'easeInOutSine' });
-	$("#slide1 > div.container > #content > .catch  > div.pointWrap > p ").delay( 3200 ).animate({ opacity: '1'},{duration:3500, easing:'easeInOutSine' });
-	$("#slide2 > div.container > #content >h1").delay( 500 ).animate({ opacity: '0.2'},{duration:3000,  easing:'easeInOutSine' });
+	$("#slide1 > div.container > #content1 >h1>img").delay( 300 ).animate({ opacity: '1'},{duration:1700, easing:'easeInOutSine' });
+	$("#slide1 > div.container > #content1 > .catch  ").delay( 1200 ).animate({ opacity: '1'},{duration:1900, easing:'easeInOutSine' });
+	$("#slide1 > div.container > #content1 > .catch  > div.pointWrap > h3").delay( 2200 ).animate({ opacity: '1',marginLeft:'0'},{ duration:600,easing:'easeInOutSine' });
+	$("#slide1 > div.container > #content1 > .catch  > div.pointWrap > p ").delay( 3200 ).animate({ opacity: '1'},{duration:3500, easing:'easeInOutSine' });
+	$("#slide2 > div.container > #content2 >h1").delay( 500 ).animate({ opacity: '0.2'},{duration:3000,  easing:'easeInOutSine' });
 
 	
 	$(window).scroll(function(){
@@ -22,8 +22,8 @@ $(function(){
 
 	
 		if(y > box1Pos.top-700){	
-			$("#slide2 > div.container > #content >h1").delay( 10 ).animate({ opacity: '1'},{duration:1000,  easing:'easeInOutSine' });
-			$("#slide2 > div.container > #content > .samplecode").delay( 1000 ).animate({ opacity: '1'},{duration:2000,  easing:'easeInOutSine' });
+			$("#slide2 > div.container > #content2 >h1").delay( 10 ).animate({ opacity: '1'},{duration:1000,  easing:'easeInOutSine' });
+			$("#slide2 > div.container > #content2 > .samplecode").delay( 1000 ).animate({ opacity: '1'},{duration:2000,  easing:'easeInOutSine' });
 		
 		}
 
@@ -31,8 +31,8 @@ $(function(){
 		
 		
 		if(y > box2Pos.top-700){			
-			$("#slide3 > div.container > #content >h1").delay( 10 ).animate({ opacity: '1'},{duration:1000,  easing:'easeInOutSine' });
-			$("#slide3 > div.container > #content > .bx-wrapper ").delay( 1000 ).animate({ opacity: '1'},{duration:2000,  easing:'easeInOutSine' });
+			$("#slide3 > div.container > #content3 >h1").delay( 10 ).animate({ opacity: '1'},{duration:1000,  easing:'easeInOutSine' });
+			$("#slide3 > div.container > #content3 > .bx-wrapper ").delay( 1000 ).animate({ opacity: '1'},{duration:2000,  easing:'easeInOutSine' });
 		}
 		
 		
