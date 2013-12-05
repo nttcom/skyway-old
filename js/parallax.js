06050ec4de74a84384f10c6e14483172198efc55
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2013-12-05 17:47:50 +0900

    パララックス効果を有効にした。

--- js/parallax.js
+++ js/parallax.js
@@ -5,13 +5,13 @@ $(function(){
 	var box2Pos = $('#slide3').offset();
 	var box3Pos = $('#footer').offset();
 
-	
+	/*
 	$("#slide1 > div.container > #content1 >h1>img").delay( 300 ).animate({ opacity: '1'},{duration:1700, easing:'easeInOutSine' });
 	$("#slide1 > div.container > #content1 > .catch  ").delay( 1200 ).animate({ opacity: '1'},{duration:1900, easing:'easeInOutSine' });
 	$("#slide1 > div.container > #content1 > .catch  > div.pointWrap > h3").delay( 2200 ).animate({ opacity: '1',marginLeft:'0'},{ duration:600,easing:'easeInOutSine' });
 	$("#slide1 > div.container > #content1 > .catch  > div.pointWrap > p ").delay( 3200 ).animate({ opacity: '1'},{duration:3500, easing:'easeInOutSine' });
 	$("#slide2 > div.container > #content2 >h1").delay( 500 ).animate({ opacity: '0.2'},{duration:3000,  easing:'easeInOutSine' });
-
+*/
 	
 	$(window).scroll(function(){
 		var y = $(this).scrollTop();
@@ -19,7 +19,7 @@ $(function(){
 		$("#slide1").css('background-position','0%' + y +'px');
 	
 	
-
+/*
 	
 		if(y > box1Pos.top-700){	
 			$("#slide2 > div.container > #content2 >h1").delay( 10 ).animate({ opacity: '1'},{duration:1000,  easing:'easeInOutSine' });
@@ -36,7 +36,7 @@ $(function(){
 		}
 		
 		
-		
+		*/
 		
 		if(y > box3Pos.top  || y < box3Pos.top){
 			$("#footer").css('background-position','50% ' + (y-box3Pos.top)*3 +'px'); 
